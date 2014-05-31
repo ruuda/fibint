788214a79beb233c48f7655bfee51bb67abc8f49
Ruud van Asseldonk <dev@veniogames.com>
2014-05-31 17:49:07 +0200

    fibonacci function done

--- main.cpp
+++ main.cpp
@@ -33,14 +33,28 @@ u64 powmod(u64 a, u64 e, u64 m)
   return r;
 }
 
-const u64 prime = 12200160415121876909;
-const u64 sqrt5 = 833731445503647576;
+u64 fib(u64 n)
+{
+  const u64 prime = 12200160415121876909;
+  const u64 sqrt5 = 833731445503647576;
+  const u64 sqrt5inv = 2606778372125104897;
+
+  n %= prime; // It overflows long before this happens, has no real use ...
+
+  u64 a = powmod(1 + sqrt5, n, prime);
+  u64 b = powmod(prime + 1 - sqrt5, n, prime);
+  u64 invpow2 = powmod(2, prime - 1 - n, prime);
+
+  u64 phin_minus_psin = (a > b) ? a - b : (prime - b) + a;
+  u64 factor = mulmod(sqrt5inv, invpow2, prime);
+
+  return mulmod(phin_minus_psin, factor, prime);
+}
 
 int main()
 {
-  u64 a, b;
-  std::cin >> a;
-  std::cin >> b;
-  std::cout << mulmod(a, b, prime);
+  u64 n;
+  std::cin >> n;
+  std::cout << fib(n);
   return 0;
 }
