c0074296e7226f3553ee0c1e18e47272a969ac35
Ruud van Asseldonk <dev@veniogames.com>
2014-06-05 16:49:22 +0200

    add traits for different integer types

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
 #include <cstdint>
 #include <iostream>
 
+typedef std::uint8_t  u8;
+typedef std::uint16_t u16;
+typedef std::uint32_t u32;
 typedef std::uint64_t u64;
 
-u64 mulmod(u64 a, u64 b, u64 m)
+template <typename T> class fib_traits { };
+
+template <> class fib_traits<u8>
 {
-  u64 r = 0;
-  u64 s = 0;
+  public:
+    static const u8 fib_max   = 13;
+    static const u8 prime     = 239;
+    static const u8 sqrt5     = 31;
+    static const u8 inv_sqrt5 = 54;
+};
+
+template <> class fib_traits<u16>
+{
+  public:
+    static const u16 fib_max   = 24;
+    static const u16 prime     = 46381;
+    static const u16 sqrt5     = 22501;
+    static const u16 inv_sqrt5 = 41605;
+};
+
+template <> class fib_traits<u32>
+{
+  public:
+    static const u32 fib_max   = 47;
+    static const u32 prime     = 2971215179;
+    static const u32 sqrt5     = 492315461;
+    static const u32 inv_sqrt5 = 692706128;
+};
+
+template <> class fib_traits<u64>
+{
+  public:
+    static const u64 fib_max   = 93;
+    static const u64 prime     = 12200160415121876909;
+    static const u64 sqrt5     = 833731445503647576;
+    static const u64 inv_sqrt5 = 2606778372125104897;
+};
+
+/* If your platform supports unsigned __int128, you can try this one as well.
+template <> class fib_traits<u128>
+{
+  public:
+    static const u128 fib_max   = 186;
+    static const u128 prime     = 332825110087067562321196029789634457911;
+    static const u128 sqrt5     = 10426323296737204245551694426008962215;
+    static const u128 inv_sqrt5 = 2085264659347440849110338885201792443;
+};
+*/
+
+template <typename T> T mulmod(T a, T b, T m)
+{
+  T r = 0;
+  T s = 0;
 
   while (b > 0)
   {
@@ -19,9 +71,9 @@ u64 mulmod(u64 a, u64 b, u64 m)
   return r;
 }
 
-u64 powmod(u64 a, u64 e, u64 m)
+template <typename T> T powmod(T a, T e, T m)
 {
-  u64 r = 1;
+  T r = 1;
   
   while (e > 0)
   {
@@ -33,17 +85,15 @@ u64 powmod(u64 a, u64 e, u64 m)
   return r;
 }
 
-u64 fib(u64 n)
+template <typename T> T fib(T n)
 {
-  const u64 prime = 12200160415121876909;
-  const u64 sqrt5 = 833731445503647576;
-  const u64 sqrt5inv = 2606778372125104897;
-
-  n %= prime; // It overflows long before this happens, has no real use ...
+  const T prime = fib_traits<T>::prime;
+  const T sqrt5 = fib_traits<T>::sqrt5;
+  const T sqrt5inv = fib_traits<T>::inv_sqrt5;
 
   u64 a = powmod(1 + sqrt5, n, prime);
   u64 b = powmod(prime + 1 - sqrt5, n, prime);
-  u64 invpow2 = powmod(2, prime - 1 - n, prime);
+  u64 invpow2 = powmod<T>(2, prime - 1 - n, prime);
 
   u64 phin_minus_psin = (a > b) ? a - b : (prime - b) + a;
   u64 factor = mulmod(sqrt5inv, invpow2, prime);
